7f3a958588aa2cd4fe6796b78e3505c55f485678
lautaro valeiras <lautta11@gmail.com>
2022-10-30 22:02:21 -0300

    innfer any types

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -6,7 +6,7 @@ export  function  UserContextProvider({children}:{children:any}){
     const [jwt, setJwt] = useState(null)
     useEffect(()=>{
         // Al ponerlo en una function obliga  a que se ejectua por unica vez
-       const token = localStorage.getItem('jwt')
+       const token:any = localStorage.getItem('jwt')
        if (token !== null) setJwt(token) 
     },[])
 
