ebda5272b00aced9ec58192b565b9c182f050a8f
lautaro valeiras <lautta11@gmail.com>
2022-10-30 23:05:39 -0300

    services

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { Environments } from "../environments/environment"
-export default function registerService({username,password}){
+export default function registerService({username,password}: {username:any, password:any}){
 
     return axios.post(`${Environments.ENDPOINT}/users/record`,{username,password}).then(res=>{
         return res
